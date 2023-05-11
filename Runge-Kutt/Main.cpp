3f53b82bb814b901f105b92a8e66f11c09fbfe63
Alexander Martynov <56801896+Alexfrom38@users.noreply.github.com>
2023-05-11 11:57:58 +0300

    Update Main.cpp
    multi-threads was added

--- Runge-Kutt/Main.cpp
+++ Runge-Kutt/Main.cpp
@@ -2,12 +2,15 @@
 #include <fstream>
 #include<cmath>
 #include<omp.h>
+#include<iomanip>
+#include<cmath>
+#define Count_of_Threads  8
 void Insert_In_File(double* new_array, size_t count, std::fstream& stream)
 {
 	if (stream.is_open())
 	{
 		for (size_t i = 0; i < count; i++)
-			stream << new_array[i] << " ";
+			stream << new_array[i] << std::setprecision(16) << " ";
 		stream << "\n";
 		stream << "\n";
 	}
@@ -15,21 +18,10 @@ void Insert_In_File(double* new_array, size_t count, std::fstream& stream)
 		throw "The file isn't exist";
 }
 
-inline double Get_A_Part_Of_Approximation(double prev_point, double curr_point, double next_point, double deltaX, double sigma, double K_i, double TimeCoef)
+inline double Get_A_Part_Of_Approximation(double prev_point, double curr_point, double next_point, double deltaX, double sigma, double K_i_prev, double K_i_curr,double K_i_next, double TimeCoef)
 {
-	double tmp_coef_with_K = K_i * TimeCoef;
-	return(  (sigma/(deltaX*deltaX)) * (prev_point+tmp_coef_with_K - 2*(curr_point+tmp_coef_with_K)+next_point+tmp_coef_with_K  ));
-}
-inline double Get_Another_A_Part_Of_Approximation(double prev_point, double curr_point, double next_point, double deltaX, double sigma, double K_i, double TimeCoef)
-{
-	double tmp_coef_with_K = K_i * TimeCoef;
-	return( tmp_coef_with_K +  (sigma / (deltaX * deltaX)) * (prev_point - 2*(curr_point)+next_point));
-}
-
-inline double Get_Another_Another_A_Part_Of_Approximation(double prev_point, double curr_point, double next_point, double deltaX, double sigma, double K_i, double TimeCoef)
-{
-	double tmp_coef_with_K = K_i * TimeCoef;
-	return( (sigma / (deltaX * deltaX)) * (prev_point - 2 * (curr_point)+next_point+tmp_coef_with_K));
+	
+	return(  (sigma/(deltaX*deltaX)) * (prev_point+(K_i_prev*TimeCoef) - 2*(curr_point)-2*(K_i_curr*TimeCoef)+next_point+(K_i_next*TimeCoef) )   );
 }
 
 inline double Get_New_Point_In_Time(double prev_point, double curr_point, double next_point, double deltaT, double deltaX, double sigma)
@@ -119,21 +111,46 @@ int main()
 		K4[0] = 0;
 		K4[count - 1] = 0;
 	
-		#pragma omp parallel for shared(temp_array,curr_array,K1,K2,K3,K4) private(a) schedule (static,8 )
-			for (a = 1; a < count - 1; a++)
-			{
-				K1[a] = Get_Another_Another_A_Part_Of_Approximation(temp_array[a - 1], temp_array[a], temp_array[a + 1], deltaX, 1, 0, 0);
-				K2[a] = Get_Another_Another_A_Part_Of_Approximation(temp_array[a - 1], temp_array[a], temp_array[a + 1], deltaX, 1, K1[a], (deltaT / 2));
-				K3[a] = Get_Another_Another_A_Part_Of_Approximation(temp_array[a - 1], temp_array[a], temp_array[a + 1], deltaX, 1, K2[a], (deltaT / 2));
-				K4[a] = Get_Another_Another_A_Part_Of_Approximation(temp_array[a - 1], temp_array[a], temp_array[a + 1], deltaX, 1, K3[a], deltaT);
-				curr_array[a] = temp_array[a] + (deltaT / 6) * (K1[a] + (2 * K2[a]) + (2 * K3[a]) + K4[a]);
-				//std::cout << a << std::endl;
-			}
-			a = 1;
+		//#pragma omp parallel for shared(temp_array,curr_array,K1,K2,K3,K4) private(a) schedule (static,8 )
+			//for (a = 1; a < count - 1; a++)
+			//{
+			//	K1[a] = Get_Another_Another_A_Part_Of_Approximation(temp_array[a - 1], temp_array[a], temp_array[a + 1], deltaX, 1, 0, 0);
+			//	K2[a] = Get_Another_Another_A_Part_Of_Approximation(temp_array[a - 1], temp_array[a], temp_array[a + 1], deltaX, 1, K1[a], (deltaT / 2));
+			//	K3[a] = Get_Another_Another_A_Part_Of_Approximation(temp_array[a - 1], temp_array[a], temp_array[a + 1], deltaX, 1, K2[a], (deltaT / 2));
+			//	K4[a] = Get_Another_Another_A_Part_Of_Approximation(temp_array[a - 1], temp_array[a], temp_array[a + 1], deltaX, 1, K3[a], deltaT);
+			//	curr_array[a] = temp_array[a] + (deltaT / 6) * (K1[a] + (2 * K2[a]) + (2 * K3[a]) + K4[a]);
+			//	//std::cout << a << std::endl;
+			//}
+			//a = 1;
+#pragma omp parallel for shared(K1,temp_array)  schedule (static,Count_of_Threads )
+		for (int a = 1; a < count - 1; a++)
+			K1[a] = Get_A_Part_Of_Approximation(temp_array[a - 1], temp_array[a], temp_array[a + 1], deltaX, 1, 0, 0, 0, 0);
+
+#pragma omp parallel for shared(K1,K2,temp_array)  schedule (static,Count_of_Threads )
+		for (int a = 1; a < count - 1; a++)
+			K2[a] = Get_A_Part_Of_Approximation(temp_array[a - 1], temp_array[a], temp_array[a + 1], deltaX, 1,K1[a - 1], K1[a], K1[a + 1], (deltaT / 2));
+
+#pragma omp parallel for shared(K2,K3,temp_array) schedule (static,Count_of_Threads )
+		for (int a = 1; a < count - 1; a++)
+			K3[a] = Get_A_Part_Of_Approximation(temp_array[a - 1], temp_array[a], temp_array[a + 1], deltaX, 1, K2[a - 1], K2[a], K2[a + 1], (deltaT / 2));
+
+#pragma omp parallel for shared(K3,K4,temp_array)  schedule (static,Count_of_Threads )
+		for (int a = 1; a < count - 1; a++)
+			K4[a] = Get_A_Part_Of_Approximation(temp_array[a - 1], temp_array[a], temp_array[a + 1], deltaX, 1, K3[a - 1], K3[a], K3[a + 1], deltaT );
+
+#pragma omp parallel for shared(temp_array,curr_array,K1,K2,K3,K4) schedule (static,Count_of_Threads )
+		for (int a = 1; a < count - 1; a++)
+			curr_array[a] = temp_array[a] + (deltaT / 6) * (K1[a] + (2 * K2[a]) + (2 * K3[a]) + K4[a]);
+		
+
 			
 			if (i % 100000 == 0)
 			{
-				//Insert_In_File(curr_array, count, f);
+			//	Insert_In_File(K1, count, f);
+				//Insert_In_File(K2, count, f);
+				//Insert_In_File(K3, count, f);
+				//Insert_In_File(K4, count, f);
+
 				std::cout << time << std::endl;
 			}
 			//for (size_t q = 0; q < count; q++)
@@ -146,6 +163,7 @@ int main()
 		//std::cout << time << std::endl;
 		i++;
 	}
+	
 	t2 = omp_get_wtime();
 	dt = t2 - t1;
 	std::cout << dt << std::endl;
@@ -153,7 +171,7 @@ int main()
 	Insert_In_File(curr_array, count, f);
 	f.close();
 	delete[] temp_array;
-	//delete[] tmp_buffer;
+	
 	delete[] curr_array;	
 	delete[] K1;
 	delete[] K2;
